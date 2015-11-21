4df91f8dc0b9fcf08f8e72910e1fb7bdab0061f9
AlbearKamoo <mrautran@gmail.com>
2015-11-21 01:18:13 -0800

    Added coin spawning

---
+++ Coinz/Assets/Coin/Setup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Setup : MonoBehaviour {
+
+    public Transform[] coins;
+
+	// Use this for initialization
+	void Awake () {
+        int size = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+public class CoinComponent
+{
+    public int value;
+    public CoinComponent() { }
+    public CoinComponent(int value)
+    {
+        this.value = value;
+    }
+}
---
+++ Coinz/Assets/Coin/Spawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+    public GameObject coin;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int x = 0; x < 5; x++)
+        {
+            GameObject coinObject = Instantiate(coin, new Vector2(x-2, 0), Quaternion.identity) as GameObject;
+            GameObject valueText = coinObject.transform.GetChild(0).gameObject;
+            int number = Random.Range(1, 10);
+            valueText.GetComponent<TextMesh>().text = number.ToString();
+        }
+    }
+      
+  
+	
+}
