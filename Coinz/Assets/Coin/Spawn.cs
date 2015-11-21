ae3bc60891e730bb8ccb176337fe9d96821a5e8a
AlbearKamoo <mrautran@gmail.com>
2015-11-21 11:17:02 -0800

    added custom variables to coin spawn

--- Coinz/Assets/Coin/Spawn.cs
+++ Coinz/Assets/Coin/Spawn.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class Spawn : MonoBehaviour {
     public GameObject coin;
+    public int coinAmount;
 
     // Use this for initialization
     void Start()
     {
-        for (int x = 0; x < 5; x++)
+        for (int x = 0; x < coinAmount; x++)
         {
-            GameObject coinObject = Instantiate(coin, new Vector2(x-2, 0), Quaternion.identity) as GameObject;
+            GameObject coinObject = Instantiate(coin, new Vector2(x-(coinAmount/2), 0), Quaternion.identity) as GameObject;
             GameObject valueText = coinObject.transform.GetChild(0).gameObject;
             int number = Random.Range(1, 10);
             valueText.GetComponent<TextMesh>().text = number.ToString();
