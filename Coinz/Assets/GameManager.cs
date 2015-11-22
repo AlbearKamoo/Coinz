0c8358716af6333ecad4d4c9bee622720fc8ebe3
AlbearKamoo <mrautran@gmail.com>
2015-11-21 20:13:50 -0800

    Improved win state

--- Coinz/Assets/GameManager.cs
+++ Coinz/Assets/GameManager.cs
@@ -6,8 +6,10 @@ public class GameManager : MonoBehaviour {
     public int playerTurn;
     [HideInInspector]
     public int destroyed;
-
+    [HideInInspector]
+    public string winner;
     public LabelManager labelManager;
+    
 
     // Use this for initialization
     void Start () {
@@ -38,8 +40,22 @@ public class GameManager : MonoBehaviour {
         destroyed += 1;
         if (destroyed == gameObject.GetComponent<Spawn>().coinAmount)
         {
-            Debug.Log("GAME OVER");
-        }
+            int player1score = int.Parse(GameObject.Find("Player1Score").GetComponent<TextMesh>().text);
+            int player2score = int.Parse(GameObject.Find("Player2Score").GetComponent<TextMesh>().text);
+            if (player1score > player2score)
+            {
+                winner = "Player 1 wins!";
+            }
+            else if(player2score > player1score)
+            {
+                winner = "Player 2 wins!";
+            }
+            else
+            {
+                winner = "It's a tie!";
+            }
+            GameObject.Find("Countdown").GetComponent<Timer>().endTimer();
+        }   
     }
 
 
--- Coinz/Assets/Stage/Timer.cs
+++ Coinz/Assets/Stage/Timer.cs
@@ -2,29 +2,39 @@
 using System.Collections;
 
 public class Timer : MonoBehaviour {
+    private bool runTimer;
     private float timer;
     public float turnTime;
 
 	// Use this for initialization
 	void Start () {
-        
+        runTimer = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        timer += Time.deltaTime;
-        float seconds = Mathf.Floor(timer % 60);
-        gameObject.GetComponent<TextMesh>().text = (turnTime - seconds).ToString("00");
-
-        if (seconds >= turnTime)
+        if (runTimer)
         {
-            GameObject.Find("GameManager").GetComponent<GameManager>().switchTurn();
-            resetTimer();
+            timer += Time.deltaTime;
+            float seconds = Mathf.Floor(timer % 60);
+            gameObject.GetComponent<TextMesh>().text = (turnTime - seconds).ToString("00");
+
+            if (seconds >= turnTime)
+            {
+                GameObject.Find("GameManager").GetComponent<GameManager>().switchTurn();
+                resetTimer();
+            }
         }
     }
 
     public void resetTimer()
     {
         timer = 0;
+    }
+
+    public void endTimer()
+    {
+        runTimer = false;
+        gameObject.GetComponent<TextMesh>().text = GameObject.Find("GameManager").GetComponent<GameManager>().winner;
     }
 }
